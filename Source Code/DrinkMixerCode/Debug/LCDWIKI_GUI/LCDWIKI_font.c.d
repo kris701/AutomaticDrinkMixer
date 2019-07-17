7a41c564bb98f707a3bb9cbfcc94f3c5c2b01917
Kristian Skov <kris701kj@gmail.com>
2019-07-17 16:45:46 +0200

    Cleaned up

--- Source Code/DrinkMixerCode/Debug/LCDWIKI_GUI/LCDWIKI_font.c.d
+++
@@ -1,2 +0,0 @@
-C:\Users\Kristian\AppData\Local\Temp\VMBuilds\DrinkMixerCode\mega_atmega2560\Debug\LCDWIKI_GUI\LCDWIKI_font.c.o: \
- C:\Users\Kristian\Documents\Arduino\libraries\LCDWIKI_GUI\LCDWIKI_font.c
--- Source Code/DrinkMixerCode/Debug/VM_DBG/VM_mem_check_sam.cpp.d
+++
@@ -1,2 +0,0 @@
-C:\Users\Kristian\AppData\Local\Temp\VMBuilds\DrinkMixerCode\mega_atmega2560\Debug\VM_DBG\VM_mem_check_sam.cpp.o: \
- c:\program\ files\ (x86)\microsoft\ visual\ studio\2019\enterprise\common7\ide\extensions\qpr3a5xw.han\Micro\ Platforms\default\debuggers\VM_DBG\VM_mem_check_sam.cpp
